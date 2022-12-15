18589d591418302bfc5432bb65e4e1277f75ffbb
Luke McGuinness <luke.mcguinness@justeattakeaway.com>
2022-12-15 16:54:09 +0000

    Small refactor

--- src/2022/Day12/Day12.cs
+++ src/2022/Day12/Day12.cs
@@ -16,10 +16,11 @@ public class Day12 : IPuzzle<Day12Args, Day12Results>
     {
         var graph = new EdgeWeightedDiGraph<Point>((d) => d.Weight <= 1);
         graph.AddEdges(input.PathGrid.Nodes.SelectMany(x => ToWeightedEdge(x.Key, x.Value, input.PathGrid)));
+       
         return new Day12Results
         {
-            Part1 = Paths.AStar(graph, input.PathGrid.Start.Item1, input.PathGrid.End.Item1, Manhattan),
-            Part2 = input.PathGrid.Nodes.Where(x => x.Value == 'a').Select(x => Paths.AStar(graph, x.Key, input.PathGrid.End.Item1, Manhattan)).Min()
+            Part1 = Paths.AStar(graph, input.PathGrid.Start.Item1, input.PathGrid.End.Item1, Manhattan).Distance,
+            Part2 = input.PathGrid.Nodes.Where(x => x.Value == 'a').Select(x => Paths.AStar(graph, x.Key, input.PathGrid.End.Item1, Manhattan).Distance).Min()
         };
     }
 
--- src/core/Paths.cs
+++ src/core/Paths.cs
@@ -3,8 +3,9 @@
     public static class Paths
     {
 
-        public static long Dijkstra<T>(EdgeWeightedDiGraph<T> graph, T start, T end) where T : notnull
+        public static (long Distance, IEnumerable<T> Paths) Dijkstra<T>(EdgeWeightedDiGraph<T> graph, T start, T end) where T : notnull
         {
+            var edges = new Dictionary<T, DirectedWeightedEdge<T>>();
             var queue = new PriorityQueue<T, long>();
             queue.Enqueue(start, 0);
             var distanceFromStart = new Dictionary<T, long> { [start] = 0 };
@@ -19,16 +20,35 @@
                     if (!distanceFromStart.TryGetValue(next.To, out long otherCost) || newCost < otherCost)
                     {
                         distanceFromStart[next.To] = newCost;
+                        edges[next.To] = next;
                         queue.Enqueue(next.To, newCost);
                     }
 
                 }
             }
-            return distanceFromStart.GetValueOrDefault(end, long.MaxValue);
+            var distance  = distanceFromStart.GetValueOrDefault(end, long.MaxValue);
+            var path = new Stack<T>();
+            if (!edges.ContainsKey(end)) return (distance, path);
+            else
+            {
+                var edge = edges[end];
+                path.Push(edge.To);
+                while(edge != null)
+                {
+                    edge = edges.ContainsKey(edge.From) ? edges[edge.From] : null;
+                    if (edge != null)
+                    {
+                        path.Push(edge.To);
+                    }
+                }
+
+            }
+            return (distance, path);
         }
 
-        public static long AStar<T>(EdgeWeightedDiGraph<T> graph, T start, T end, Func<T, T, long> heurisitc) where T : notnull
+        public static (long Distance, IEnumerable<T> Paths) AStar<T>(EdgeWeightedDiGraph<T> graph, T start, T end, Func<T, T, long> heurisitc) where T : notnull
         {
+            var edges = new Dictionary<T, DirectedWeightedEdge<T>>();
             var queue = new PriorityQueue<T, long>();
             queue.Enqueue(start, 0);
 
@@ -48,13 +68,32 @@
                     if (!distanceFromStart.TryGetValue(next.To, out long otherCost) || newCost < otherCost)
                     {
                         distanceFromStart[next.To] = newCost;
+                        edges[next.To] = next;
                         var h = heurisitc.Invoke(next.To, end);
                         var priority = newCost + h;
                         queue.Enqueue(next.To, priority);
                     }
                 }
             }
-            return distanceFromStart.GetValueOrDefault(end, long.MaxValue);
+            var distance = distanceFromStart.GetValueOrDefault(end, long.MaxValue);
+            var path = new Stack<T>();
+            if (!edges.ContainsKey(end)) return (distance, path);
+            else
+            {
+                var edge = edges[end];
+                path.Push(edge.To);
+                while (edge != null)
+                {
+                    edge = edges.ContainsKey(edge.From) ? edges[edge.From] : null;
+                    if(edge != null)
+                    {
+                        path.Push(edge.To);
+                    }
+                   
+                }
+
+            }
+            return (distance, path);
         }
 
        
